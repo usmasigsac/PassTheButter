b3906604492707da4a6332de0718eec7c741f424
stonepresto <prestonpdppritchard@gmail.com>
2017-12-02 01:20:52 -0500

    adding working loader

--- loader/loader.py
+++ loader/loader.py
@@ -11,7 +11,7 @@ import subprocess
 from threading import Thread
 import py_compile
 import time
-import importlib
+import importlib.util
 import multiprocessing as mp
 #############
 
@@ -22,183 +22,180 @@ def async(f):
     return wrapper
 
 class Job:
-    def __init__(self,name,loader):
-        self.name = name
-        self.payload = importlib.abc.SourceLoader(self.name)
-        self.path = name + '/'
-        self.threads = {}
-        self.stations = [] #managed by launcher
-        self.interval = 0
-        self.enabled = False
-        self.lastRun = 0
-        self.flags = []
+  def __init__(self,name,loader):
+    self.name = name
+    self.payload = importlib.util.spec_from_file_location("pwn",name)
+    self.threads = {}
+    self.stations = [] #managed by launcher
+    self.interval = 1
+    self.enabled = False
+    self.lastRun = 0
+    self.flags = {}
+    #self.service = getattr(self.payload,"SERVICE")
 
-    def load(self):
-        """
-        loads from backup
+  def writeLog(self,string):
+    loader.log.write("[#] %s"%string)
 
-        :return:
-        """
-        pass
+  def enable(self):
+    self.enabled = True
 
-    def export(self):
-        """
-        writes to backup
+  def disable(self):
+    self.enabled = False
 
-        :return:
-        """
-        pass
+  def changeStations(self,newstations):
+    self.stations = newstations
 
-    def writeLog(self,string):
-        loader.log.write("[#] %s"%string)
+  def changeInterval(self,newinterval):
+    self.interval = newinterval
 
-    def enable(self):
-        self.enabled = True
+  def stop(self):
+    try:
+      loader.jobs.remove(self.name)
+    except:
+      print("[#] Error stopping %s"%self.name)
 
-    def disable(self):
-    	self.enabled = False
+  def newFlags(self,job_id,flags,service):#hook
+    pass
 
-    def changeStations(self,newstations):
-        self.stations = newstations
-
-    def changeInterval(self,newinterval):
-        self.interval = newinterval
-
-    def stop(self):
-    	try:
-        	loader.jobs.remove(self.name)
-        except:
-        	print("[#] Error stopping %s"%self.name)
-
-    def newFlags(self,job_id,flags):#hook
-    	pass
-
-    def run(self):
-    	try:
-	        if hasattr(self.payload, 'pwn'):
-	            for team in self.stations:
-	                flag = getattr(self.payload,'pwn')(team) #user inputs a function 'pwn' that returns the flag
-	                self.flags.insert(0,flag)
-	                self.newFlags(self.name,self.flags)
-	    except:
-	    	print("[!!] Error during %s runtime"%self.name)
+  def run(self):
+    try:
+      if hasattr(self.payload, 'pwn'):
+        for ip in self.stations:
+          flag = getattr(self.payload,'pwn')(ip) #user inputs a function 'pwn' that returns the flag
+          self.flags[ip] = flag
+        self.newFlags(self.name,self.flags,self.service)
+    except:
+      print("[!!] Error during %s runtime"%self.name)
 
 class Pool:
-	def __init__(self,pooldir,loader):
-		self.location = pooldir
-		self.loader = loader
+  def __init__(self,pooldir,loader):
+    self.location = pooldir
+    self.loader = loader
 
-	def get():
-		return subprocess.getoutput(['ls'+self.location]).split('\n')
-	
-	def check():
-		diff = []
-		for file in self.get():
-			if file not in self.loader.loaded:
-				diff.insert(0,file)
-		return diff
+  def get(self):
+    return subprocess.getoutput(['ls '+self.location]).split('\n')
+  
+  def check(self):
+    diff = []
+    for file in self.get():
+      if file not in self.loader.loaded and "pycache" not in file and "init" not in file:
+        diff.insert(0,file)
+    return diff
 
 class Loader:
-	def __init__(self,pooldir,config_file,log_file):
-		print("[#] Initializing ...")
-		self.pool = Pool(pooldir,self)
-		self.load = pool.get()
-		self.loaded = []
-		self.jobs = []
-		self.config = config_file
-		self.log = open(log_file,'w')
-		self.exitstatus = 0
+  def __init__(self,pooldir,config_file='test.cfg',log_file='loader.log'):
+    self.pool = Pool(pooldir,self)
+    self.load = self.pool.get()
+    self.loaded = []
+    self.jobs = []
+    self.config = config_file
+    self.log = open(log_file,'w')
+    self.exitstatus = 0
+    self.payload_path = ""
 
-	def exception(self,exception,location):
-		print("[!!] %s during %s" % (exception,location))
+  def exception(self,exception,location):
+    print("[!!] %s during %s" % (exception,location))
 
-	def requirements(self,env,dir):
-		#NOTE: the name of the .txt is the install env to be used.
-		#For example, if you needed pwntools, you would simply
-		#put 'pwntools' on it's own line in pip.txt.
-		#The 'env' parameter specifies which install environment
-		#to use.
-		reqs_f = open((dir+'/%s.txt' % env),'r')
-		reqs = []
-		for line in reqs_f:
-			if '' == subprocess.getoutput('which '+line.strip()):
-				reqs.insert(0,line.strip()) #reqs is mutable, no need to reassign
-				print("[#] Added %s to install queue..." % line.strip())
-		for req in reqs:
-			try:
-				os.system(env+" install " +req)
-				print("[#] Installing requirement %s..."%req)
-			except KeyboardInterrupt:
-				self.log.write("[#] Exited on Ctrl-C")
-				exit(0)
-			except:
-				exception(sys.exc_info()[0],"requirements installation")
-				err = "[!!] Ignoring %s"%req
-				print(err)
-				self.log.write(err)
-				continue
-		print("[#] Requirements installed.")
+  def requirements(self,env,dir):
+    #NOTE: the name of the .txt is the install env to be used.
+    #For example, if you needed pwntools, you would simply
+    #put 'pwntools' on it's own line in pip.txt.
+    #The 'env' parameter specifies which install environment
+    #to use.
+    reqs_f = open((dir+'/%s.req' % env),'r')
+    reqs = []
 
-	def newjobs(self,boolean,jobs):#hook
-		pass
+    for line in reqs_f:
+      if '' == subprocess.getoutput('which '+line.strip()):
+        reqs.insert(0,line.strip()) #reqs is mutable, no need to reassign
+        print("[#] Added %s to install queue..." % line.strip())
+    for req in reqs:
+      try:
+        os.system(env+" install " +req)
+        print("[#] Installing requirement %s..."%req)
+      except KeyboardInterrupt:
+        self.log.write("[#] Exited on Ctrl-C")
+        exit(0)
+      except:
+        exception(sys.exc_info()[0],"requirements installation")
+        err = "[!!] Ignoring %s"%req
+        print(err)
+        self.log.write(err)
+        continue
+    print("[#] Requirements installed.")
 
-	def checkfile(self):
-		pass
+  def newjobs(self,boolean,jobs):#hook
+    pass
 
-	def run(self):
-		try:
-			while True:
-				new = self.pool.check()
-				if new != []:
-					os.system('unzip '+' '.join(newfile for newfile in new))
-					for newfile in new:
-						#name of zip, folder, and payload
-						newdir = newfile.split('.zip')[0]
-						env = subprocess.getoutput('ls *.req').split('.req')[0]
-						requirements(env,newdir) #install new requirements
+  def checkfile(self):
+    pass
 
-						#new thread in case installing takes a bit
-						try:
-							if env == 'pip':
-								@async
-								py_compile.compile('%s.py' % newdir)
-							else:
-								@async
-								os.system('gcc -o %s %s.c' % (newdir,newdir))
+  def run(self):
+    try:
+      while True:
+        new = self.pool.check()
+        if new != []:
+          for newfile in new:
+            os.system('unzip -qq -u pool/'+newfile+" -d pool/")
+            #name of zip, folder, and payload
+            file = newfile.split('.zip')[0]
+            newdir = "pool/"+file
+            os.system('touch '+newdir+"/__init__.py")
+            env = subprocess.getoutput('ls '+newdir+'/*.req').split('.req')[0]
+            env = env.split(newdir+"/")[1]
+            self.requirements(env,newdir) #install new requirements
 
-						except KeyboardInterrupt:
-							self.log.write("[#] Exited on Ctrl-C")
-							exit(0)							
-						except:
-							exception(sys.exc_info()[0],"on %s's installation" % newdir)
-							print("[!!] %s not compiled." % newdir)
-							continue
+            #new thread in case installing takes a bit
+            try:
+              if env == 'pip':
+                #@async
+                py_compile.compile('%s.py' % (newdir+"/"+file))
+                os.system('cp '+newdir+'/__pycache__/'+file+".cpython-34.pyc "+newdir+"/pwn.pyc")
+              else:
+                #@async
+                os.system('gcc -o %s %s.c' % (newdir+file,newdir+file))
 
-						#init job
-						newjob = Job(newdir,self)
-						self.jobs.insert(0,newjob)
-					self.log.write("[#] New jobs created.")
-					self.newjobs(True,self.jobs)
+            except KeyboardInterrupt:
+              self.log.write("[#] Exited on Ctrl-C")
+              exit(0)             
+            except:
+              self.exception(sys.exc_info()[0],"on %s's installation" % file)
+              print("[!!] %s not compiled." % file)
+              continue
 
-		except KeyboardInterrupt:
-			self.log.write("[#] Exited on Ctrl-C")
-			exit(0)
-		except:
-			self.log.write("[!!] %s at %s" % (sys.exc_info()[0],time.time()))
-			wait = True
-			while wait:
-				input = input('> ')
-				if input[0] == "r":
-					self.run()
-					wait = False
-				elif input[0] == "c":
-					wait = False
-				else:
-					print("[#] Please reboot or continue.")
-					sleep(1)
+            #init job
+            newjob = Job(newdir+"/pwn.pyc",self)
+            self.jobs.insert(0,newjob)
+            print("[#] Job < %s > added." % file)
+          self.log.write("[#] New jobs created.")
+          self.newjobs(True,self.jobs)
+
+    except KeyboardInterrupt:
+      self.log.write("[#] Exited on Ctrl-C")
+      exit(0)
+    # except:
+    #   self.log.write("[!!] %s at %s" % (sys.exc_info()[0],time.time()))
+    #   wait = True
+    #   while wait:
+    #     inp = input('> ')
+    #     if inp[0] == "r":
+    #       self.run()
+    #       wait = False
+    #     elif inp[0] == "p":
+    #       print(sys.exc_info())
+    #       wait = False
+    #     elif inp[0] == "e":
+    #       wait = False
+    #     else:
+    #       print("[#] Please reboot or continue.")
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("[#] Usage: loader.py <config.cfg> <loader.log>")
-	loader = Loader("pool/",pid,sys.argv[1],sys.argv[2])
-	loader.run()
+#  try:
+    loader = Loader("pool/",sys.argv[1],sys.argv[2])
+    loader.run()
+  # except:
+  #   if len(sys.argv) != 3:
+  #     print("[#] Usage: loader.py <config.cfg> <loader.log>")
+    # else:
+    #   print("[!!] Error:")
+    #   print(sys.exc_info())
